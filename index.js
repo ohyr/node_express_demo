63310ca0b5ef161c62affb7c60b13e187de1be90
Yongrok Oh <ohyr96@ajou.ac.kr>
2021-06-11 18:05:14 +0900

    Connect MongoDB

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const { USER_NAME, PASSWORD } = require("./privacy/mongo");
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    `mongodb+srv://${USER_NAME}:${PASSWORD}@boilerplate.zjthq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
