22e22a8b4c89d647d8408accce784dd9bfe3c650
Yongrok Oh <ohyr96@ajou.ac.kr>
2021-06-22 00:39:22 +0900

    Add login

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cookieParser = require("cookie-parser");
 
 const config = require("./config/key");
 
@@ -8,6 +9,7 @@ const { User } = require("./models/User");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 mongoose
@@ -31,6 +33,35 @@ app.post("/register", (req, res) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
+    });
+  });
+});
+
+app.post("/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "User not found.",
+      });
+    }
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "Invalid password.",
+        });
+      }
+
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        res.cookie("x_auth", user.token).status(200).json({
+          loginSuccess: true,
+          userId: user._id,
+        });
+      });
     });
   });
 });
