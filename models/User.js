26c2491494c4265022841047aea4f7de803de68b
Yongrok Oh <ohyr96@ajou.ac.kr>
2021-06-21 23:52:44 +0900

    Fix: Pre user schema error

--- models/User.js
+++ models/User.js
@@ -36,17 +36,19 @@ const userSchema = mongoose.Schema({
 userSchema.pre("save", function (next) {
   const user = this;
 
-  if (user.isModified("password") === false) return;
-
-  bcrypt.genSalt(saltRounds, function (err, salt) {
-    if (err) return next(err);
-
-    bcrypt.hash(user.password, salt, function (err, hash) {
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
       if (err) return next(err);
-      user.password = hash;
-      next();
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
     });
-  });
+  } else {
+    next();
+  }
 });
 
 const User = mongoose.model("User", userSchema);
