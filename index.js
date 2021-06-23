ac0e0a6ab6048ca48ba9e94179be694ff18fcc43
Yongrok Oh <ohyr96@ajou.ac.kr>
2021-06-23 23:32:00 +0900

    Change end point name

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
 
   user.save((err, doc) => {
@@ -37,7 +37,7 @@ app.post("/register", (req, res) => {
   });
 });
 
-app.post("/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
       return res.json({
